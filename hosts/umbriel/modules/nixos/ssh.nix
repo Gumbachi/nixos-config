f7f5598394b830f7da784dc405449efd4daf580e
Gumbachi <github@gumbachi.com>
2025-12-21 02:52:19 -0500

    disable password auth

--- hosts/umbriel/modules/nixos/ssh.nix
+++ hosts/umbriel/modules/nixos/ssh.nix
@@ -1,10 +1,10 @@
 { ... }: {
 
   users.users.jared.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDunG/8V8/5tvAJdVpYqoMtb4FaI6LYgie/5VmmS5ChH jared@Enceladus"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDunG/8V8/5tvAJdVpYqoMtb4FaI6LYgie/5VmmS5ChH jared@enceladus"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKy7se5rdlXmFkrWzfQj1Uej19mCupQDMoD67TJ7TBA"
   ];
 
-  services.openssh.settings.PasswordAuthentication = true;
+  services.openssh.settings.PasswordAuthentication = false;
 
 }
